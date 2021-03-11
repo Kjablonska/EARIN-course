b1029343a4ddb499ec44aa1874a208769b48962c
Wojciech Marosek <wojtek.marosek@gmail.com>
2021-03-11 19:57:17 +0100

    structure changed

---
+++ ex1_common_methods.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from ex1_Newton_method import newton_based_method
+from ex1_gradient_based_method import gradient_based_method
+from ex1_tui import print_error
+
+
+def matrix_j(x, A, b, c):
+    a1 = np.dot(b.transpose(), x)
+    a2 = np.dot(np.dot(x.transpose(), A), x)
+    return c + a1 + a2
+
+
+def grad(x, A, b):
+    return 2*np.dot(A, x) + b
+
+
+def batch_mode_method(method, N, A, b, cur_x):
+    sol = []
+    if method == 'gradient':
+        for i in range(N):
+            sol.append(gradient_based_method(A, b, cur_x))
+
+    if method == 'newton':
+        for i in range(N):
+            sol.append(newton_based_method(A, b, cur_x))
+
+    print("Mean value of the result from", N, "iterations:", np.mean(sol))
+    print("Standard deviation:", np.std(sol))
+
+
+def is_symmetric(M):
+    print(M.transpose().all() == M.all())
+    return M.transpose().all() == M.all()
+
+
+def is_positive_definite(M):
+    try:
+        np.linalg.cholesky(M)
+        return True
+    except np.linalg.LinAlgError:
+        error = 'Defined input variables are incorrect! Please define valid one!'
+        print_error(error)
--- ex1_gradient_based_method.py
+++ ex1_gradient_based_method.py
@@ -1,31 +1,11 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
-import sys
 
-# Change it so as params are provided by the user.
-A = np.asarray([[2, 0], [0, 2]])
-b = np.asarray([1, 2])
-c = 2
-N = 10
-
-def J(x,A,c):
-    a1 = np.dot(b.transpose(), x)
-    a2 = np.dot(np.dot(x.transpose(), A), x)
-    return c + a1 + a2
-
-def grad(x,A,b):
-    return 2*np.dot(A, x) + b
+from ex1_common_methods import grad
 
 
-def gradientBasedMethod(A,b,cur_x):
-    if len(cur_x) == 2:
-        print(cur_x[0])
-        print(cur_x[1])
-        cur_x = np.random.uniform(int(cur_x[0]), int(cur_x[1]), b.size)
-
-    if len(cur_x) == 1:
-        cur_x = int(cur_x[0])
+def gradient_based_method(A, b, current_sol):
+    current_sol = np.random.uniform(int(current_sol[0]), int(current_sol[1]), b.size)
 
     rate = 0.01                                 # Learning rate.
     precision = 0.0000001                       # Precision of the solution.
@@ -37,18 +17,10 @@ def gradientBasedMethod(A,b,cur_x):
 
     start_time = time.time()
     while max(step_size) > precision and iter < max_iter and exe_time < max_exe_time:
-        prev_x = cur_x                                  # Storing value of current x as a prev.
-        cur_x = cur_x - grad(prev_x, A, b) * rate
-        step_size = abs(cur_x - prev_x)                 # Change in x
+        prev_x = current_sol                                  # Storing value of current x as a prev.
+        current_sol = current_sol - grad(prev_x, A, b) * rate
+        step_size = abs(current_sol - prev_x)                 # Change in x
         iter = iter + 1
         exe_time = time.time() - start_time
 
-    return cur_x
-
-def batchMode(N, A, b,cur_x):
-    sol = []
-    for i in range(N):
-        sol.append(gradientBasedMethod(A, b,cur_x))
-
-    print("Mean value of the reult from", N, "iterations:", np.mean(sol))
-    print("Standard deviation:", np.std(sol))
+    return current_sol
