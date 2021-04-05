0eecd73c2f366dfd86bbba0c06b9e499e270f9ef
Kjablonska <k.jablonska8@gmail.com>
2021-04-06 01:44:13 +0200

    ex3 done

--- ex3/3b.pl
+++ ex3/3b.pl
@@ -1,11 +1,7 @@
-find_triplets(A, B, C) :-
+pythagorean_triplets(A, B, C) :-
     between(1, 20, A),
     between(1, 20, B),
     A < B,
     between(1, 20, C),
     B < C,
     C*C =:= A*A + B*B.
-
-is_triple(A, B, C) :-
-  D is C*C - A*A - B*B,  % must use "is" for arithmetic
-  D = 0.
