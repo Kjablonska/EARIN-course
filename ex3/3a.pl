ca3f14f6a334f4f33555ab42976866b09f2a2eb6
Kjablonska <k.jablonska8@gmail.com>
2021-04-08 12:34:04 +0200

    small improvement

--- ex3/3a.pl
+++ ex3/3a.pl
@@ -1,12 +1,8 @@
-# swpl
-# to compile: ['path'].
-# to run isA0(0, 2, 0, 0, 0, 0).
-
 isA0(A0, A1, A2, A3, A4, A5) :-
     A01 is A1/2,
     A02 is A2/4,
     A03 is A3/8,
     A04 is A4/16,
     A05 is A5/32,
-    S is A0 + A01 + A02 + A03 + A04 + A05, write(S), nl,
+    S is A0 + A01 + A02 + A03 + A04 + A05,
     S >= 1.
--- ex3/3c.pl
+++ ex3/3c.pl
@@ -1,11 +1,8 @@
-% https://stackoverflow.com/questions/2817848/find-pythagorean-triplet-for-which-a-b-c-1000
-
-triplets(A, B, C) :-
-    between(1, 333, A),
-    between(1, 500, B),
+pythagorean_triplets(A, B, C) :-
+    between(1, 1000, A),
+    between(1, 1000, B),
     A < B,
     C is 1000 - A - B,
     B < C,
-    1000 =:= A + B + C,
     C*C =:= A*A + B*B,
     write("(A="), write(A), write(", B="), write(B), write(", C="), write(C), write(")"), nl, fail.
