114b823199db2ef0698c897bbc73ea63c0c2e2ae
Kjablonska <k.jablonska8@gmail.com>
2021-05-20 18:40:03 +0200

    svm

--- ex6/svm.py
+++ ex6/svm.py
@@ -1,5 +1,5 @@
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix
 
 from validate_data import validate_data
 
@@ -9,6 +9,7 @@ def svm(X_train, y_train, X_test, y_test):
     pred_svc = svc.predict(X_test)
     print("=======================================")
     print("SVM")
+    print(confusion_matrix(y_test, pred_svc))
     print(classification_report(y_test, pred_svc))
 
     validate_data(y_test, X_test, pred_svc)
