2186cd44eecf6cd1322351d4a0dea89ef1868bb8
Wojciech Marosek <wojtek.marosek@gmail.com>
2021-06-01 22:16:14 +0200

    comments

--- ex7/main.py
+++ ex7/main.py
@@ -6,30 +6,42 @@ from common import LEARN_COEFF, EPOCHS, BATCH_SIZE, LIMIT, SAMPLES, function
 from net import Net
 
 def main():
+    # -------------------------------------------------------------------------------------
+    #   Stochastic gradient descent
+    # -------------------------------------------------------------------------------------
     network = Net()
-    optim = torch.optim.SGD(network.parameters(), lr=LEARN_COEFF, momentum=0.9)
-    criterion = torch.nn.MSELoss()
+    optim = torch.optim.SGD(network.parameters(), lr=LEARN_COEFF, momentum=0.9)     # Create a optimizer with momentum
+    criterion = torch.nn.MSELoss()                                                  # Mean Squared Error Loss
+
     X = torch.from_numpy(np.linspace(LIMIT[0], LIMIT[1], SAMPLES)).float().unsqueeze(1)
     Y = function(X)
     dataloader = DataLoader(TensorDataset(X, Y), batch_size=BATCH_SIZE, shuffle=True)
 
+    # -------------------------------------------------------------------------------------
+    #   Training algorithm
+    # -------------------------------------------------------------------------------------
     loss_vals = []
     for epoch in range(EPOCHS):
         error = []
         for batch, expected in dataloader:
+            # Feeding forward
             loss = criterion(network(batch), expected)
             optim.zero_grad()
+
+            # Backward propagation
             loss.backward()
             optim.step()
             error.append(loss.detach().numpy())
 
         if epoch % 100 == 0:
             print("epoch {}: {}".format(epoch, loss.item()))
+
         loss_vals.append(np.mean(error))
     predict = network(X)
 
-
-    # Plot function and predicted values.
+    # -------------------------------------------------------------------------------------
+    #   Plot function and predicted values.
+    # -------------------------------------------------------------------------------------
     x, y = X.detach().numpy(), Y.detach().numpy()
     plt.plot(x, y, label="function")
     plt.plot(x, predict.detach().numpy(), label="predicted")
@@ -40,7 +52,9 @@ def main():
     plt.savefig(fname="EARIN_ex7_prediction.png", figsize=[10, 10])
     plt.show()
 
+    # -------------------------------------------------------------------------------------
     # Plot mean loss vs epoch.
+    # -------------------------------------------------------------------------------------
     plt.plot(range(EPOCHS), loss_vals)
     plt.title("Mean loss")
     plt.xlabel("iterations")
--- ex7/net.py
+++ ex7/net.py
@@ -1,7 +1,10 @@
 from torch.utils.data import DataLoader, TensorDataset
 from torch import nn
-
+# -------------------------------------------------------------------------------------
+#   Two layer model
+# -------------------------------------------------------------------------------------
 class Net(nn.Module):
+    # Constructor
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
@@ -10,5 +13,6 @@ class Net(nn.Module):
             nn.Linear(650, 1),
         )
 
+    # Prediction
     def forward(self, X):
         return self.net(X)
