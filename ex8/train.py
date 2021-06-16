b69f0a38f9e257bfbd634a41962e326c563051e5
Wojciech Marosek <wojtek.marosek@gmail.com>
2021-06-16 18:00:31 +0200

    changes

--- ex8/train.py
+++ ex8/train.py
@@ -2,22 +2,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import gym
 import random
-from constants import TEST_EPISODES, MAX_STEPS, TRAIN_EPISODES, ALPHA, GAMMA, EPS, MAX_EPSILON, MIN_EPSILON, DECAY_RATE, MODEL_FILE
+from constants import TRAIN_EPISODES, ALPHA, GAMMA, EPS, MAX_EPSILON, MIN_EPSILON, DECAY_RATE, MODEL_FILE, MAX_STEPS
 
 
 def create_enivronment():
     env = gym.make("Taxi-v3")
     action_size = env.action_space.n
     state_size = env.observation_space.n
-    print("Action space size: ", action_size)
-    print("State space size: ", state_size)
+
+    print("=   Action space size: ", action_size)
+    print("=   State space size: ", state_size)
+
     return env, action_size, state_size
 
 
 def plot_data(training_rewards):
-    print("Training score over time: " +
+    print("=   Training score over time: " +
           str(sum(training_rewards)/TRAIN_EPISODES))
     x = range(TRAIN_EPISODES)
+
     plt.plot(x, training_rewards)
     plt.xlabel('episode')
     plt.ylabel('Training cumulative reward')
@@ -34,37 +37,44 @@ def main():
     env = gym.make("Taxi-v3")
     action_size = env.action_space.n
     state_size = env.observation_space.n
-    print("Action space size: ", action_size)
-    print("State space size: ", state_size)
+
+    print("=   Action space size: ", action_size)
+    print("=   State space size: ", state_size)
+
+    # Create Q matrix [500 x 6] of zeros, 500 states, 6 actions
     Q = np.zeros((state_size, action_size))
     training_rewards = []
     epsilon = EPS
 
     for episode in range(TRAIN_EPISODES):
+        # Resets the environment and returns a random initial state.
         state = env.reset()
         cumulative_training_rewards = 0
         done = False
 
-        while not done:
-            # Choosing an action among the possible states.
-            # If this number > epsilon, select the action corresponding to the biggest Q value for this state (Exploitation)
+        for step in range(MAX_STEPS):
+            # Decide whether to pick a random action or to exploit the already computed Q-values (Exploitation).
+            # Prevent Q-values tendency of converge
             if random.uniform(0, 1) > epsilon:
                 action = np.argmax(Q[state, :])
-            # Choose a random action (Exploration)
             else:
                 action = env.action_space.sample()
 
+            # env.step - step the environment by one timestep. reward - if action is beneficial or not
             new_state, reward, done, info = env.step(action)
 
-            # Update the Q array.
+            # Update the Q array. Calculate the maximum Q-value for the actions corresponding to the next_stat
             # Bellman equation:
             Q[state, action] = (1 - ALPHA) * Q[state, action] + ALPHA * \
                 (reward + GAMMA * np.max(Q[new_state, :]) - Q[state, action])
             cumulative_training_rewards += reward
             state = new_state
 
-        print("Cumulative reward for episode {}: {}".format(
+            # The end of the peisode.
+            if done == True:
+                print("=   Cumulative reward for episode {}: {}".format(
                     episode, cumulative_training_rewards))
+                break
 
 
         # Reduce epsilon since there is a need for less exploration each time.
